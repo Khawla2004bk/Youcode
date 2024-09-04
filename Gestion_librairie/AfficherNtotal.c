4115733f442aa2cfd0fe62d9b0cdc2086a576f9e
Khawla Boukniter <125459327+Khawla2004bk@users.noreply.github.com>
2024-09-04 10:35:40 +0100

    Update AfficherNtotal.c

--- Gestion_librairie/AfficherNtotal.c
+++ Gestion_librairie/AfficherNtotal.c
@@ -1,13 +1,16 @@
 #include <header.h>
 
 void AfficherNtotal() {
-  int count = 0;
+  int count = 0, s = 0;
   
   for (int i = 0; i < nb; i++) {
     if (Q[i] == 0) 
       continue;
-    else
+    else {
       count++;
+      s += Q[i];
+    }
   }
-  printf("%d", count);
+  printf("Le nombre de livres en tock est %d", count);
+  printf("La quantité total de tous les livres en tock est %d", s);
 }
