7cee89653823d5ca43b09b996e915491321b0317
boukniter khawla <boukniter.khawla@gmail.com>
2024-09-05 12:39:36 +0100

    fonction de multiplication

---
+++ Day02/fonctions/challenge2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int mult(int a, int b){
+    return a * b;
+}
+int main() {
+    int a, b;
+
+    printf("Entrez un nombre : ");
+    scanf("%d", &a);
+    printf("Entrez un nombre : ");
+    scanf("%d", &b);
+
+    printf("le produit est: %d", mult(a, b));
+
+    return 0;
+}
