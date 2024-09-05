130905124fc4020d3464551efddfb1d40dada85e
boukniter khawla <boukniter.khawla@gmail.com>
2024-09-05 10:43:07 +0100

    challenge de la recherche dichotomique

---
+++ Day02/boucles/challenge8.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main() {
+    int i = 0, n, nb, end, mil, rslt;
+
+    printf("Donner la taille du tableau: ");
+    scanf("%d", &n);
+
+    end = n - 1;
+    int T[n];
+
+    for (i = 0; i < n; i++) {
+        printf("Entrer la valeur %d : ", i+1);
+        scanf("%d", &T[i]);
+    }
+
+    printf("Entrer la valeur à rechercher: ");
+    scanf("%d", &nb);
+
+    while (i <= end) {
+        mil = (i + end) / 2;
+        if (T[mil] == nb) {
+            rslt = mil;
+            break;
+        }
+
+        else if (T[mil] < nb)
+            i = mil + 1;
+
+        else
+            end = mil - 1;
+    }
+
+    if (rslt != -1)
+        printf("valeur trouvée");
+    else
+        printf("valeur non trouvée");
+
+    return 0;
+}
