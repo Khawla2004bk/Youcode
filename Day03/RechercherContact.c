8058336f89f102ddb096df43a125862f7a666641
boukniter khawla <boukniter.khawla@gmail.com>
2024-09-05 17:09:00 +0100

    ajouter la fonction RechercherContact

--- Day03/RechercherContact.c
+++ Day03/RechercherContact.c
@@ -4,5 +4,13 @@ void RechercherContact() {
     char nom[30];
 
     printf("Enter le nom du contact: ");
-    
+    scanf("%[^\n]", nom);
+    getchar();
+
+    for (i = 0 ; i < nb; i++) {
+        if (strcmp(cont[i].N, nom) == 0) {
+            printf("Contact trouvé:\n\tNom: %s | Tel: %s | Email: %s\n", nom, cont[i].T, cont[i].E);
+            break;
+        }
+    }
 }
