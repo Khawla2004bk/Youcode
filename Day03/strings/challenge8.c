c6e8199f37baa50ea5b54ca021eefc1d4cdd1775
Khawla Boukniter <boukniter.khawla@gmail.com>
2024-09-07 14:04:22 +0100

    ajouter le challenge 8 qui permet de convertir une chaine de caractères en minuscule

---
+++ Day03/strings/challenge8.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main() {
+    char str[50];
+
+    printf("Entrer  une chaîne de caractères:\n\t");
+    scanf("%[^\n]", str);
+    getchar();
+
+    printf("La chaîne de caractères en majuscule est:\n\t");
+
+    for (int i = 0; i < strlen(str); i++) {
+        printf("%c", tolower(str[i]));
+    }
+
+    return 0;
+}
