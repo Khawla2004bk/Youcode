7f3b8d2476c553c6fcf39d6a31627032ae733516
boukniter khawla <boukniter.khawla@gmail.com>
2024-09-11 09:27:22 +0100

    Ajout d'une confirmation pour la suppression d'un étudiant avec options "oui" ou "non"

--- Gestion_etudiants/SupprimerEtudiant.c
+++ Gestion_etudiants/SupprimerEtudiant.c
@@ -1,20 +1,31 @@
 #include "header.h"
 
 void SupprimerEtudiant() {
-    int id;
+    int id, choix;
 
     printf("Entrer l'id de l'etudiant que vous voulez supprimer: ");
     scanf("%d", &id);
     getchar();
 
-    for (int i = 0; i < nb; i++) {
-        if (etudiant[i].ID == id) {
-            for (int j = i; j < nb - 1; j++) {
-                etudiant[j] = etudiant[j + 1];
+    printf("Voulez-vous confirmer la suppression de l'etudiant?\n");
+    printf("\t1. Oui\n\t2. Non\n");
+    scanf("%d", &choix);
+    getchar();
+
+    if (choix == 1) {
+        for (int i = 0; i < nb; i++) {
+            if (etudiant[i].ID == id) {
+                for (int j = i; j < nb - 1; j++) {
+                    etudiant[j] = etudiant[j + 1];
+                }
+                nb--;
+                printf("Etudiant supprime avec succes!\n");
+                break;
             }
-            nb--;
-            printf("Etudiant supprime avec succes!\n");
-            break;
         }
+    }
+
+    else if (choix == 2) {
+        printf("Suppression annulee!\n");
     }
 }
